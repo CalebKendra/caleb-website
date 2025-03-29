68276112fead3f0f701246256eb401a5343d22e8
CalebKendra <calebkendra7@gmail.com>
2025-03-28 21:23:34 -0400

    update with intro code

--- src/index.js
+++ src/index.js
@@ -9,14 +9,17 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "static")));
 
-app.get('/', (req, res) => {
+app.get("/:page?", (req, res) => {
+  const page = req.params.page || "index";
+  let filePath = path.join(__dirname, `pages/${page}.html`);
+
   try {
-    res.status(200).sendFile(path.join(__dirname, "pages/index.html"));
+    res.status(200).sendFile(filePath);
   } catch (e) {
     console.error("Error Fetching Index File:", e);
     res.status(500).send(e.toString());
   }
-})
+});
 
 app.listen(PORT, (e) => {
   if (e) console.log(e);
