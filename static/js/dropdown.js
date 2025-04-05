5863020163b657d80fb20c85114cd889aeff362f
CalebKendra <calebkendra7@gmail.com>
2025-04-04 22:33:37 -0400

    added baseline dropdown menu

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,8 +1,17 @@
-let dropdownToggle = document.querySelector('.dropdown-button')
-let bars = document.querySelectorAll('.dropdown-bar')
+let dropdownToggle = document.querySelector('.dropdown-button');
+let dropdownContent = document.querySelector('.dropdown-content');
+let bars = document.querySelectorAll('.dropdown-bar');
 
 function toggleHamburger(e) {
-  bars.forEach(bar => bar.classList.toggle('dropdown-x'))
+  if (!bars[0].classList.contains('dropdown-x')) {
+    dropdownContent.setAttribute("style", "display: flex;")
+    dropdownToggle.setAttribute("style", "position: fixed;")
+  } else {
+    dropdownContent.setAttribute("style", "display: none;")
+    dropdownToggle.setAttribute("style", "position: absolute;")
+  }
+
+  bars.forEach(bar => bar.classList.toggle('dropdown-x'));
 }
 
-dropdownToggle.addEventListener('click', toggleHamburger)
+dropdownToggle.addEventListener('click', toggleHamburger);
