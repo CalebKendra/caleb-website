0e99c79279fb7fc028ccfd436831fdae35cbd775
CalebKendra <calebkendra7@gmail.com>
2025-04-06 17:56:16 -0400

    added home picture animation

--- static/js/typing.js
+++ static/js/typing.js
@@ -5,6 +5,13 @@ const sentences = [
     "50% Bodybuilder"
 ];
 
+const calebBridge = document.getElementById('Bridge');
+const calebHat = document.getElementById('Hat');
+const calebCatch = document.getElementById('Catch');
+const calebWind = document.getElementById('Wind');
+
+const images = [calebBridge, calebHat, calebCatch, calebWind];
+
 let currentSentence = 0;
 let currentChar = 0;
 let isDeleting = false;
@@ -13,11 +20,22 @@ const typingSpeed = 100;
 const deletingSpeed = 50;
 const delayBetweenSentences = 1000;
 
+function fadeIn(element, progress) {
+    element.style.opacity = progress;
+}
+
+function fadeOut(element, progress) {
+    element.style.opacity = progress;
+}
+
 function type() {
     const currentText = sentences[currentSentence];
+    const currentImage = images[currentSentence];
+
     if (isDeleting) {
         typewriterElement.textContent = currentText.substring(0, currentChar - 1);
         currentChar--;
+        fadeOut(currentImage, + currentChar / (currentText.length - 5));
         if (currentChar === 0) {
             isDeleting = false;
             currentSentence = (currentSentence + 1) % sentences.length;
@@ -28,6 +46,7 @@ function type() {
     } else {
         typewriterElement.textContent = currentText.substring(0, currentChar + 1);
         currentChar++;
+        fadeIn(currentImage, 0.2 + currentChar / currentText.length);
         if (currentChar === currentText.length) {
             isDeleting = true;
             setTimeout(type, delayBetweenSentences);
