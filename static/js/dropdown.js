7c8c5791e3414ca8a49d68184e2cb3f9aa1dc0e6
CalebKendra <calebkendra7@gmail.com>
2025-04-05 21:35:12 -0400

    removed index link from dropdown

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,3 +1,4 @@
+let dropdownHeader = document.getElementById('dropdown-header');
 let dropdownToggle = document.querySelector('.dropdown-button');
 let dropdownContent = document.querySelector('.dropdown-content');
 let bars = document.querySelectorAll('.dropdown-bar');
@@ -6,9 +7,11 @@ function toggleHamburger(e) {
   if (!bars[0].classList.contains('dropdown-x')) {
     dropdownContent.setAttribute("style", "display: flex;")
     dropdownToggle.setAttribute("style", "position: fixed;")
+    dropdownHeader.setAttribute("style", "color: var(--white);")
   } else {
     dropdownContent.setAttribute("style", "display: none;")
     dropdownToggle.setAttribute("style", "position: absolute;")
+    dropdownHeader.setAttribute("style", "color: var(--black);")
   }
 
   bars.forEach(bar => bar.classList.toggle('dropdown-x'));
@@ -20,6 +23,7 @@ window.addEventListener('resize', () => {
   if (window.innerWidth >= 680 && dropdownContent.style.display == "flex") {
     dropdownContent.setAttribute("style", "display: none;")
     dropdownToggle.setAttribute("style", "position: absolute;")
+    dropdownHeader.setAttribute("style", "color: var(--black);")
 
     bars.forEach(bar => bar.classList.toggle('dropdown-x'));
   }
