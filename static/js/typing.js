2083a05fa1f611806044dd222151674862ecc308
CalebKendra <calebkendra7@gmail.com>
2025-04-06 11:35:18 -0400

    added typewriter effect to home

---
+++ static/js/typing.js
@@ -0,0 +1,43 @@
+const sentences = [
+    "Tech Enthusiast",
+    "Full-Stack Developer",
+    "div Centerer",
+    "50% Goalkeeper",
+    "50% Bodybuilder"
+];
+
+let currentSentence = 0;
+let currentChar = 0;
+let isDeleting = false;
+const typewriterElement = document.getElementById('typewriter');
+const typingSpeed = 100;
+const deletingSpeed = 50;
+const delayBetweenSentences = 1000;
+
+function type() {
+    const currentText = sentences[currentSentence];
+    if (isDeleting) {
+        typewriterElement.textContent = currentText.substring(0, currentChar - 1);
+        currentChar--;
+        if (currentChar === 0) {
+            isDeleting = false;
+            currentSentence = (currentSentence + 1) % sentences.length;
+            setTimeout(type, typingSpeed);
+        } else {
+            setTimeout(type, deletingSpeed);
+        }
+    } else {
+        typewriterElement.textContent = currentText.substring(0, currentChar + 1);
+        currentChar++;
+        if (currentChar === currentText.length) {
+            isDeleting = true;
+            setTimeout(type, delayBetweenSentences);
+        } else {
+            setTimeout(type, typingSpeed);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(type, typingSpeed);
+});
